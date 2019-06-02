14549fedd74c827ae6666047a7f33771b99ca99c
Trance <xp46290@gmail.com>
2019-06-02 12:33:58 -0400

    Intial Commit

---
+++ test2/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  test2
+//
+//  Created by Mark Trance on 6/2/19.
+//  Copyright © 2019 Trance Aerospace. All rights reserved.
+//
+
+import Foundation
---
+++ test2/ItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemViewController.swift
+//  test2
+//
+//  Created by Mark Trance on 6/2/19.
+//  Copyright © 2019 Trance Aerospace. All rights reserved.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("item vc loaded")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
